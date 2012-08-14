c475944eb67a22fc91d92a49ba09755ba268be85
root <root@localhost.localdomain>
2012-08-13 21:30:08 -0400

    Removed python install script.
    Fixed several problems with install.sh and got it to
    "work" at a minimal level.

--- releng/root-image/root/Desktop/install.sh
+++ releng/root-image/root/Desktop/install.sh
@@ -17,6 +17,9 @@ do
 			if [ "$param" == "" ] && [ "$curr_attr" == "required" ]; then
 				echo "You must set "$var" in install.conf."
 				exit 1
+			elif [ "$var" == "efi_partition" ] && [ -n "$param" ] && [ "$bootloader_package" == "grub-efi-x86_64" ]; then
+				echo "You must specify the efi partition if you are using grub-efi-x86_64."
+				exit 1
 			fi
 			eval $var='$param'
 		fi
@@ -31,10 +34,14 @@ echo "Starting..."
 # step 2: mount
 echo "Mounting drives..."
 mount $root_partition /mnt
-mkdir /mnt/boot
-mount $boot_partition /mnt/boot
-mkdir /mnt/home
-mount $home_partition /mnt/home
+if [ -n "$boot_partition" ]; then
+	mkdir /mnt/boot
+	mount $boot_partition /mnt/boot
+fi
+if [ -n "$home_partition" ]; then
+	mkdir /mnt/home
+	mount $home_partition /mnt/home
+fi
 
 # step 3: install base packages
 echo "Installing packages..."
@@ -47,29 +54,35 @@ pacstrap /mnt $bootloader_package
 # step 5: configure
 echo "Configuring..."
 genfstab -p /mnt >> /mnt/etc/fstab
-arch-chroot /mnt << EOF
-echo $hostname > /etc/hostname
-ln -s /usr/share/zoneinfo/$locale_zone/$locale_subzone /etc/localtime
+echo $hostname > /mnt/etc/hostname
+ln -s /mnt/usr/share/zoneinfo/$locale_zone/$locale_subzone /mnt/etc/localtime
 safe_locale_preference=$(printf "%s\n" "$locale_preference" | sed 's/[][\.*^$/]/\\&/g')
-sed -i "s/#$safe_locale_preference/$safe_locale_preference/" /etc/locale.gen
-locale-gen
-mkinitcpio -p linux
+sed -i "s/#$safe_locale_preference/$safe_locale_preference/" /mnt/etc/locale.gen
+arch-chroot /mnt /bin/bash -c "locale-gen"
+arch-chroot /mnt /bin/bash -c "mkinitcpio -p linux"
 
 # step 5.1: configure bootloader
 echo "Configuring bootloader..."
-modprobe dm-mod
-grub-install /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
+mkdir -p /mnt/boot/grub/locale
+cp /mnt/usr/share/locale/en@quot/LC_MESSAGES/grub.mo /mnt/boot/grub/locale/en.mo
+arch-chroot /mnt /bin/bash -c "modprobe dm-mod"
+
+if [ "$bootloader_package" == 'grub-bios' ]; then
+	drive=$(echo $boot_partition | sed -e 's/[0-9]//')
+	arch-chroot /mnt /bin/bash -c "grub-install $drive"
+else
+	arch-chroot /mnt /bin/bash -c "mkdir -p /boot/efi"
+	arch-chroot /mnt /bin/bash -c "mount -t vfat $efi_partition /boot/efi"
+	arch-chroot /mnt /bin/bash -c "grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck --debug"
+fi
+arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
 
 echo "Set new root password:"
-passwd
+arch-chroot /mnt /bin/bash -c "passwd"
 
-useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/bash $default_user
+arch-chroot /mnt /bin/bash -c "useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/bash $default_user"
 echo "Set default user password:"
-passwd $default_user
-
-exit
-EOF
+arch-chroot /mnt /bin/bash -c "passwd $default_user"
 
 # step 6: unmount/cleanup
 echo "Unmounting..."
